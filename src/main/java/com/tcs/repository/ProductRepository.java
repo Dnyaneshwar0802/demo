26ac1bb687323c4d6226dc1d408b864c4fd62bb2
GroupProject-D <dnyaneshwarbagal0802@gmail.com>
2025-07-21 12:03:34 +0530

    Convert ProductRepository to MongoRepository interface
    Refactored ProductRepository from a class to an interface extending MongoRepository for Product entities. This enables Spring Data MongoDB support and provides CRUD operations out of the box.

--- src/main/java/com/tcs/repository/ProductRepository.java
+++ src/main/java/com/tcs/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.tcs.repository;
 
-public class ProductRepository {
+import com.tcs.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
 }
